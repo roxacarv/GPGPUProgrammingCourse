9b8e3b886215970bf0b335e70790c480ecc5fed6
roxac <xavier.rc791@gmail.com>
2017-11-24 21:51:08 -0300

    Changing of project type (IDE/OS switch)
    Projects changed from Visual Studio projects to Eclipse Nsight projects. Code was partially re-written to work with Linux-based distros and all the code originally from Windows platform was removed.

---
+++ Codes/Exercise 1/DevicePropertiesViewer/Debug/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../src/devinfo.cu 
+
+OBJS += \
+./src/devinfo.o 
+
+CU_DEPS += \
+./src/devinfo.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/opt/cuda/bin/nvcc -G -g -O0 -gencode arch=compute_30,code=sm_30  -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	/opt/cuda/bin/nvcc -G -g -O0 --compile --relocatable-device-code=false -gencode arch=compute_30,code=compute_30 -gencode arch=compute_30,code=sm_30  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
