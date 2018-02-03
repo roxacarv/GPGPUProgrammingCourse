0a20928e06f6505563dc3fee96f517d36ddb8bbf
roxac <xavier.rc791@gmail.com>
2018-02-03 20:02:23 -0300

    new folder organization

---
+++ Codes/gpu-cuda-c-exercises/src/addvectors.cu
@@ -0,0 +1,62 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+#define N 10
+
+__global__ void add(int *a, int *b, int *c) {
+	int tid = blockIdx.x;
+	if (tid < N)
+		c[tid] = a[tid] + b[tid];
+}
+
+int main(void) {
+
+	int a[N], b[N], c[N], userInput, numToAdd;
+	int *dev_a, *dev_b, *dev_c;
+	srand((unsigned)time(NULL));
+
+	cudaMalloc((void**)&dev_a, N * sizeof(int));
+	cudaMalloc((void**)&dev_b, N * sizeof(int));
+	cudaMalloc((void**)&dev_c, N * sizeof(int));
+
+	//receive input from user
+	printf("Choose the numbers for the arrays\n");
+	printf("1 to fill automatically or 2 to fill manually: ");
+	scanf("%d", &userInput);
+
+	//process user input
+	if (userInput == 1) {
+		for (int i = 0; i < N; i++) {
+			a[i] = rand() % 20;
+			b[i] = rand() % 10;
+		}
+	} else {
+		for (int i = 0; i < N; i++) {
+			printf("Choose a number %d for A: ", i+1);
+			scanf("%d", &numToAdd);
+			a[i] = numToAdd;
+			printf("Choose a number %d for B: ", i+1);
+			scanf("%d", &numToAdd);
+			b[i] = numToAdd;
+		}
+	}
+
+	cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
+
+	add<<<N, 1 >>>(dev_a, dev_b, dev_c);
+
+	cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+	//free unused memory
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_c);
+}
---
+++ Codes/gpu-cuda-c-exercises/src/inner_product.cu
@@ -0,0 +1,57 @@
+#include "../common/book.h"
+
+#define imin(a,b) (a<b?a:b)
+
+const int N = 33 * 1024;
+const int threadsPerBlock = 256;
+const int blocksPerGrid = imin(32, (N+threadsPerBlock-1) / threadsPerBlock);
+
+__global__ void dot(float *a, float *b, float *c)
+{
+	__shared__ float cache[threadsPerBlock];
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	int cacheIndex = threadIdx.x;
+	float temp = 0;
+	while (tid < N) {
+		temp += a[tid] * b[tid];
+		tid += blockDim.x * gridDim.x;
+	}
+
+	cache[cacheIndex] = temp;
+	int i = blockDim.x/2;
+	while (i != 0)
+	{
+		if (cacheIndex < i)
+			cache[cacheIndex] += cache[cacheIndex + 1];
+		__syncthreads();
+		i /= 2;
+	}
+}
+
+int main(void)
+{
+	float *a, *b, c, *partial_c;
+	float *dev_a, *dev_b, *dev_partial_c;
+	a = new float[N];
+	b = new float[N];
+	partial_c = new float[blocksPerGrid];
+
+	cudaMalloc((void**)&dev_a, N*sizeof(float));
+	cudaMalloc((void**)&dev_b, N*sizeof(float));
+	cudaMalloc((void**)&dev_partial_c, blocksPerGrid*sizeof(float));
+
+	for (int i = 0; i < N; i++) {
+		a[i] = i;
+		b[i] = i * 2;
+	}
+
+	cudaMemcpy(dev_a, a, N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, b, N*sizeof(float), cudaMemcpyHostToDevice);
+
+	dot<<<blocksPerGrid, threadsPerBlock>>>(dev_a, dev_b, dev_partial_c);
+
+	cudaMemcpy(partial_c, dev_partial_c, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost);
+	c = 0;
+	for (int i = 0; i < blocksPerGrid; i++)
+		c += partial_c[i];
+}
